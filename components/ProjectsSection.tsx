e84185e69b662c3539280d93cd402d993b8f03dc
Joana Santos <97113028+joanamcs@users.noreply.github.com>
2023-06-05 12:00:17 +0100

    feat: update design and photos (#1)
    * feat: upgrade next and design
    
    * feat: update photos

--- components/ProjectsSection.tsx
+++ components/ProjectsSection.tsx
@@ -2,31 +2,25 @@ import React from "react"
 import Image from "next/image"
 import Link from "next/link"
 import SlideUp from "./SlideUp"
-import { BsGithub, BsArrowUpRightSquare } from "react-icons/bs"
+import {BsGithub, BsArrowUpRightSquare} from "react-icons/bs"
 
-const projects = [
+type Projects = {
+  id: string,
+  name: string,
+  description: string,
+  image: string,
+  link: string,
+  github?: string,
+}
+
+const projects: Array<Projects> = [
   {
-    name: "Thankful Thoughts",
+    id: 'wtx',
+    name: "WTX",
     description:
-      "ThankfulThoughts is a web app that generates an appreciative sentence of something or someone you are thankful for.",
-    image: "/thankfulthoughts.png",
-    github: "https://github.com/hqasmei/thankful-thoughts",
-    link: "https://thankfulthoughts.io/",
-  },
-  {
-    name: "PlatoIO",
-    description: "PlatoIO is a to do list app that built using the PERN stack.",
-    image: "/platoio.png",
-    github: "https://github.com/hqasmei/platoio",
-    link: "https://platoio.com/register",
-  },
-  {
-    name: "Kator Family Photos",
-    description:
-      "Kator Family Photos is a photos and video digitization service in the LA area.",
-    image: "/familyphotos.png",
-    github: "https://github.com/hqasmei/katorfamilyphotos",
-    link: "https://katorfamilyphotos.com/",
+      "Changing the way trucks are traded over the globe by simplifying the process while adding more security and efficiency.",
+    image: "/wtxsite.png",
+    link: "https://wtx.com/",
   },
 ]
 
@@ -35,13 +29,13 @@ const ProjectsSection = () => {
     <section id="projects">
       <h1 className="my-10 text-center font-bold text-4xl">
         Projects
-        <hr className="w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded"></hr>
+        <hr className="w-6 h-1 mx-auto my-4 bg-secondary-light dark:bg-secondary-dark border-0 rounded"></hr>
       </h1>
 
       <div className="flex flex-col space-y-28">
-        {projects.map((project, idx) => {
+        {projects.map((project) => {
           return (
-            <div key={idx}>
+            <div key={project.id} className={'pb-8'}>
               <SlideUp offset="-300px 0px -300px 0px">
                 <div className="flex flex-col  animate-slideUpCubiBezier animation-delay-2 md:flex-row md:space-x-12">
                   <div className=" md:w-1/2">
@@ -57,16 +51,16 @@ const ProjectsSection = () => {
                   </div>
                   <div className="mt-8 md:w-1/2">
                     <h1 className="text-4xl font-bold mb-6">{project.name}</h1>
-                    <p className="text-xl leading-7 mb-4 text-neutral-600 dark:text-neutral-400">
+                    <p className="text-xl leading-7 mb-4 text-neutral-700 dark:text-neutral-100">
                       {project.description}
                     </p>
                     <div className="flex flex-row align-bottom space-x-4">
-                      <Link href={project.github} target="_blank">
+                      {!!project.github && <Link href={project.github} target="_blank">
                         <BsGithub
                           size={30}
                           className="hover:-translate-y-1 transition-transform cursor-pointer"
                         />
-                      </Link>
+                      </Link>}
                       <Link href={project.link} target="_blank">
                         <BsArrowUpRightSquare
                           size={30}
@@ -80,7 +74,7 @@ const ProjectsSection = () => {
             </div>
           )
         })}
-        
+
       </div>
     </section>
   )
