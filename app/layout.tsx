8355f90bfe64207479c7c5122debee9d72d84ab5
Joana Santos <j.santos@kigroup.de>
2023-06-13 11:40:51 +0100

    feat: add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "../styles/globals.css"
 import Navbar from "@/components/Navbar"
 import Footer from "@/components/Footer"
 import {ThemeProvider} from "next-themes"
+import {Analytics} from "@vercel/analytics/react";
 
 export default function RootLayout({children}: {
   children: React.ReactNode
@@ -19,6 +20,7 @@ export default function RootLayout({children}: {
       <Navbar/>
       {children}
       <Footer/>
+      <Analytics/>
     </ThemeProvider>
     </body>
     </html>
